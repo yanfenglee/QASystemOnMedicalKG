9f4b734d8f46dd3f99ea6abb97acf7f4ba588dd0
liyanfeng <yanfeng.li@medishare.cn>
2019-08-10 09:56:13 +0800

    change config

--- build_medicalgraph.py
+++ build_medicalgraph.py
@@ -15,8 +15,8 @@ class MedicalGraph:
         self.g = Graph(
             host="127.0.0.1",  # neo4j 搭载服务器的ip地址，ifconfig可获取到
             http_port=7474,  # neo4j 服务器监听的端口号
-            user="lhy",  # 数据库user name，如果没有更改过，应该是neo4j
-            password="lhy123")
+            user="neo4j",  # 数据库user name，如果没有更改过，应该是neo4j
+            password="neo4j")
 
     '''读取文件'''
     def read_nodes(self):
